919180e80a689f8df303284fc7837130ffbd25ac
Javid-concept <javid1541@gmail.com>
2023-01-15 00:46:50 -0600

    clean unneeded

--- config.mk
+++ config.mk
@@ -1,11 +1,5 @@
 build/%.o: cs225/%.cpp Makefile
 	@${compiler} $(filter-out ${Warnings},${Compiler_Flags}) $< -o $@ && echo -n "${Cyan}($(patsubst build/%.o,%,$@))${Nrml}"
 
-build/cs225_intro.o: src/cs225_intro.cpp Makefile
-	@${compiler} $(filter-out -Wconversion,${Compiler_Flags}) $< -o $@ && echo -n "${Cyan}($(patsubst build/%.o,%,$@))${Nrml}"
-
-build/cs225_ArtHelper.o: src/cs225_ArtHelper.cpp Makefile
-	@${compiler} $(filter-out -Wconversion,${Compiler_Flags}) $< -o $@ && echo -n "${Cyan}($(patsubst build/%.o,%,$@))${Nrml}"
-
 build/lodepng.o: cs225/lodepng/lodepng.cpp Makefile
 	@${compiler} $(filter-out ${Warnings},${Compiler_Flags}) $< -o $@ && echo -n "${Cyan}($(patsubst build/%.o,%,$@))${Nrml}"
